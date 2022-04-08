3b65920e467f37497bf0c3adef479bc489e68a65
Andrey Seredyuk <90606940+AdamRain94@users.noreply.github.com>
2022-04-09 01:07:12 +0300

    update

--- src/main/resources/static/js/chat.js
+++ src/main/resources/static/js/chat.js
@@ -14,11 +14,9 @@ $(function(){
     function addOneMessage(message){
 
         let messageDiv = $('<div class="message"></div>');
-        let messageInfo = $('<div class="message-info"><div class="user-name flex"><p class="name" id="' + message.id + '" style="color: rgb(' + message.users.color + '); "><b>' + message.users.name + '</p><p class="time">[' + new Date(new Date(message.dateTime).setHours(new Date(message.dateTime).getHours() + 3)).toLocaleTimeString() + ']</p></div></div>');
+        let messageInfo = $('<div class="message-info"><div class="user-name flex"><p class="name" id="' + message.id + '" style="color: rgb(' + message.users.color + '); "><b>' + message.users.name + '</p><p class="time">[' + new Date(message.dateTime).toLocaleTimeString() + ']</p></div></div>');
         let messageText = $('<div class="message-text"><p class="text">' + message.message + '</p></div>');
-                                                                                                                                                                                                                                    consoleLog(new Date().getTimezoneOffset());
-                                                                                                                                                                                                                                    consoleLog(new Date(message.dateTime).toLocaleTimeString())
-                                                                                                                                                                                                                                    consoleLog(message.dateTime);
+
         messageDiv.append(messageInfo, messageText);
         $('.windows-messages').append(messageDiv);
         $('.windows-messages').scrollTop($('.windows-messages').prop('scrollHeight'));
